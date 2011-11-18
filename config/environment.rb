f54d7e65432fa361ea864f5c0a82b5a5bc85a6f4
JamesF <james@freelanceful.com>
2011-11-18 05:53:10 -0600

    Fixing precompile stuff for Heroku to work

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SampleApp::Application.initialize!
 
-# Added in to make Heroku work with Assets Pipeline
-# http://stackoverflow.com/questions/7443536/blueprint-screen-css-isnt-precompiled
-config.assets.precompile += %w( *.css *.js )
-
